c35ac453c4f4de1825fa76d1341013e8168bbc0e
unknown <messagethompson@gmail.com>
2021-09-30 10:24:11 -0700

    adding game engine structure to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 #include "RenderTutorial.h"
 
+int initialize()
+{
+    // when the engine starts
+
+    return 0;
+}
+
+int runEngine()
+{
+    // check input
+    // run physics
+    // run ECS
+    // render
+
+    return 0;
+}
+
+int teardown()
+{
+    // when the engine closes
+
+    return 0;
+}
+
 int main() {
+
+    initialize();
+
     RenderTutorial();
+
+    // should try to get the code below working to make a more general game loop?
+
+    //// keep the window open if it's not supposed to close
+    //while (!glfwWindowShouldClose(window))
+    //{
+    //    // tell glfw to keep track of window resize 
+    //    // and input events
+    //    glfwPollEvents();
+    //    runEngine();
+    //}    
+
+    teardown();
+
     return 0;
 }
