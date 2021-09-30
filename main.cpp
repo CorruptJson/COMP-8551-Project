b680eb177bc6d3a148477f9f1ca522b6b7ca0a82
unknown <messagethompson@gmail.com>
2021-09-30 11:08:32 -0700

    changed render tutorial to fit with main.cpp init,run,teardown structure

--- main.cpp
+++ main.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 #include "RenderTutorial.h"
 
+// gets called once when engine starts
+// put initilization code here
 int initialize()
 {
     // when the engine starts
+    renderTutorialInit();
 
     return 0;
 }
 
+// the main update function
+// game loop will be put here eventually
 int runEngine()
 {
     // check input
     // run physics
     // run ECS
     // render
+    renderTutorialUpdate();
 
     return 0;
 }
 
+
+// gets called once when engine ends
+// put teardown code here
 int teardown()
 {
     // when the engine closes
+    renderTutorialTeardown();
 
     return 0;
 }
@@ -29,18 +39,14 @@ int main() {
 
     initialize();
 
-    RenderTutorial();
-
-    // should try to get the code below working to make a more general game loop?
-
-    //// keep the window open if it's not supposed to close
-    //while (!glfwWindowShouldClose(window))
-    //{
-    //    // tell glfw to keep track of window resize 
-    //    // and input events
-    //    glfwPollEvents();
-    //    runEngine();
-    //}    
+    // keep the window open if it's not supposed to close
+    while (!glfwWindowShouldClose(window))
+    {
+        // tell glfw to keep track of window resize 
+        // and input events
+        glfwPollEvents();
+        runEngine();
+    }    
 
     teardown();
 
