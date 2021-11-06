b652d79b28c92d048986f11355fadf2b1d113c18
unknown <messagethompson@gmail.com>
2021-11-05 17:59:14 -0700

    fixing entity queries

--- EntityQuery.cpp
+++ EntityQuery.cpp
@@ -22,14 +22,13 @@ void EntityQuery::searchChunks(std::vector<Chunk*>& allChunks)
 
         int k = 0;
 
-        for (int j = 0; j < compTypes.size(); j++)
+        for (int j = 0; j < compTypes.size() && k < archTypes.size(); j++)
         {
-            if (compTypes[j] != archTypes[k])
+            for (; k < archTypes.size(); k++)
             {
-                k++;
-                if (k == archTypes.size())
+                if (compTypes[j] == archTypes[k])
                 {
-                    continue;
+                    break;
                 }
             }
         }
