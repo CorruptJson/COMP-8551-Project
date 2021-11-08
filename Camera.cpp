be929f1e64895fbc37368cbfc5a1b8de3e04a127
Thomas Bui <thomasbui198@gmail.com>
2021-11-07 17:59:15 -0800

    Refactor camera to contain projection matrix

--- Camera.cpp
+++ Camera.cpp
@@ -1,27 +1,36 @@
 #include "Camera.h"
 
-Camera::Camera(float posX, float posY, float rotZ, float zoomFactor) {
-    position = vec2(posX, posY);
+Camera::Camera(float posX, float posY) {
+    position = glm::vec2(posX, posY);
+    projectionMatrix = glm::mat4(1);
 }
 
-Camera::Camera() {
-    position = vec2(0.0f, 0.0f);
-}
+Camera::Camera() : Camera(0, 0) { }
 
+void Camera::setViewSize(int viewWidth, int viewHeight) {
+    float CENTER_X_COORD = 0;
+    float CENTER_Y_COORD = 0;
+    float LEFT_X_COORD = CENTER_X_COORD - viewWidth / 2;
+    float RIGHT_X_COORD = CENTER_X_COORD + viewWidth / 2;
+    float BOTTOM_Y_COORD = CENTER_Y_COORD - viewHeight / 2;
+    float TOP_Y_COORD = CENTER_Y_COORD + viewHeight / 2;
+    float EYE_NEAR = 0.f;
+    float EYE_FAR = -1.f;
+    projectionMatrix = glm::ortho(LEFT_X_COORD, RIGHT_X_COORD, BOTTOM_Y_COORD, TOP_Y_COORD, EYE_NEAR, EYE_FAR);
+}
+    
 void Camera::moveCamera(float x, float y) {
     position.x += x;
     position.y += y;
 }
 
-mat4 Camera::getViewMatrix() {
-    mat4 modelMatrix = mat4(1.0f);
-    return translate(modelMatrix, vec3(position.x, position.y, 0));
+// note that when the camera moves, we move the whole world
+// in the opposite direction.
+glm::mat4 Camera::getViewMatrix() {
+    glm::mat4 modelMatrix = glm::mat4(1.0f);
+    return glm::translate(modelMatrix, glm::vec3(-position.x, -position.y, 0));
 }
 
-//void Camera::rotateCamera(float x, float y) {
-//
-//}
-//
-//void Camera::zoom(float factor) {
-//
-//}
+glm::mat4 Camera::getProjectionMatrix() {
+    return projectionMatrix;
+}
